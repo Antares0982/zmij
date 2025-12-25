ba7caab66cd2a802a43df586745bdcd31a5d080b
Victor Zverovich <viz@meta.com>
2025-12-24 18:14:05 -0800

    Parallelize verification program (thanks to dtolnay)

--- test/verify.cc
+++ test/verify.cc
@@ -6,31 +6,51 @@
 #include <stdio.h>   // printf
 #include <string.h>  // memcpy
 
-#include "dragonbox/dragonbox_to_chars.cpp"
+#include <thread>
+#include <vector>
 
+#include "dragonbox/dragonbox_to_chars.h"
 #include "zmij.h"
 
 int main() {
-  char actual[zmij::float_buffer_size] = {};
-  char expected[32] = {};
-  uint32_t i = 0;
-  double percent = 100.0 / (1LL << 32);
-  do {
-    if (i % 50'000'000 == 0) printf("Progress: %.2f%%\n", i * percent);
-
-    uint32_t bits = i++;
-    float value = 0;
-    memcpy(&value, &bits, sizeof(float));
-
-    zmij::write(actual, sizeof(actual), value);
-    *jkj::dragonbox::to_chars(value, expected) = '\0';
-
-    if (strcmp(actual, expected) == 0) continue;
-    if (strcmp(actual, "0") == 0 && strcmp(expected, "0e0") == 0) continue;
-    if (strcmp(actual, "-0") == 0 && strcmp(expected, "-0e0") == 0) continue;
-
-    printf("Output mismatch: %s != %s\n", actual, expected);
+  unsigned concurrency = std::thread::hardware_concurrency();
+  constexpr unsigned long long N = std::numeric_limits<uint32_t>::max() + 1ULL;
+  uint32_t n = static_cast<uint32_t>(N / concurrency);
+  if (static_cast<uint64_t>(n) * concurrency != N) {
+    printf("Unsupported concurrency\n");
     return 1;
-  } while (i != 0);
-  printf("Tested %u values\n", unsigned(i));
+  }
+
+  std::vector<std::thread> threads(concurrency);
+  for (uint32_t i = 0; i < concurrency; ++i) {
+    threads[i] = std::thread([i, n] {
+      char actual[zmij::float_buffer_size] = {};
+      char expected[32] = {};
+      double percent = 100.0 / n;
+      for (uint32_t j = 0; j < n; ++j) {
+        if (j % 50'000'000 == 0)
+          printf("Thread %d progress: %.2f%%\n", i, j * percent);
+
+        uint32_t bits = i * n + j;
+
+        float value = 0;
+        memcpy(&value, &bits, sizeof(float));
+
+        zmij::write(actual, sizeof(actual), value);
+        *jkj::dragonbox::to_chars(value, expected) = '\0';
+
+        if (strcmp(actual, expected) == 0) continue;
+        if (strcmp(actual, "0") == 0 && strcmp(expected, "0e0") == 0) continue;
+        if (strcmp(actual, "-0") == 0 && strcmp(expected, "-0e0") == 0)
+          continue;
+
+        printf("Output mismatch: %s != %s\n", actual, expected);
+        return;
+      }
+      while (i != 0);
+    });
+  }
+
+  for (int i = 0; i < concurrency; ++i) threads[i].join();
+  printf("Tested %llu values\n", N);
 }
